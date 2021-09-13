7719bed6eff6eafab0727cc94dec7a698a1a8a4e
taha.shaikh <taha.shaikh@actonate.com>
2021-09-14 00:19:45 +0530

    insight fixed

--- src/components/Home/Insights/InsightBlock.tsx
+++ src/components/Home/Insights/InsightBlock.tsx
@@ -20,8 +20,8 @@ const InsightBlock = (props: IInsightBlock) => {
         </div>
       )}
       <div
-        className={`w-full lg:w-1/2 flex items-start content-center justify-center flex-wrap space-y-5 xs:order-2 lg:order-none${
-          isLeft ? ' lg:pl-20' : ' lg:pr-20'
+        className={`w-full lg:w-1/2 flex items-start content-center justify-center flex-wrap space-y-5${
+          isLeft ? ' lg:pl-20' : ' lg:pr-20 xs:order-1 lg:order-0'
         }`}
       >
         <p className="w-full font-medium text-3xl text-customColors-100 xs:mt-5 lg:mt-0">{name}</p>
@@ -35,7 +35,7 @@ const InsightBlock = (props: IInsightBlock) => {
         </a>
       </div>
       {!isLeft && (
-        <div className="w-full lg:flex-1 flex items-center justify-center lg:-mr-32">
+        <div className="w-full lg:flex-1 flex items-center justify-center lg:-mr-32 xs:order-0 lg:order-1">
           <img src={src} className="w-full h-full object-contain" />
         </div>
       )}
