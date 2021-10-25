fae1130eef8d658c527cdc8d010f96deed4b147b
taha.shaikh <taha.shaikh@actonate.com>
2021-10-25 15:28:04 +0530

    build fix

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ImgContainer from '../components/Careers/ImgContainer';
 import OverView from '../components/Careers/OverView';
-import { WorkingBenifits } from '../components/Careers/workingBenifits';
 import WorkWithUs from '../components/Home/WorkWithUs';
 import Layout from '../components/Layout/layout';
 
@@ -10,7 +9,6 @@ const Careers = () => {
     <Layout>
       <ImgContainer />
       <OverView />
-      <WorkingBenifits />
       <WorkWithUs />
     </Layout>
   );
