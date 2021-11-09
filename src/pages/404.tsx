3a97f6a5010313296c2d5e09cc4b07bbe90e4b20
taha.shaikh <taha.shaikh@actonate.com>
2021-11-09 10:56:09 +0530

    bug fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ErrorPage = () => (
-  <div className="flex justify-center items-center w-full h-full">
+  <div className="flex justify-center items-center w-full h-full flex-wrap">
     <h1>The page you are looking for is not found</h1>
   </div>
 );
