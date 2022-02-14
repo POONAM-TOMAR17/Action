d81f3ae642f83050824a926960c53c9bba065d28
Poonam Tomar <poonam.tomar@actonate.com>
2022-02-14 15:31:41 +0530

    hero banner height is fixed

--- src/components/Home/HeroSlider.tsx
+++ src/components/Home/HeroSlider.tsx
@@ -8,7 +8,7 @@ const HeroSlider = () => {
         {/* Slider */}
         <div
           className="w-full h-full relative main-slider swiper-container"
-          // style={{ height: '723x' }}
+          style={{ height: '600px' }}
         >
           {mainSlider.map(
             (item, index) =>
